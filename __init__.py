c38493396cf810bdc7083b9cf3d5933b3e602616
codeKID <acuaytv@gmail.com>
2018-01-13 13:41:11 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,31 @@
+import os
+import util.getarguments
+import sys
+import glob
+from util.getarguments import *
+
+def initiate():
+  if os.path.isdir("./page"):
+    print("Folder PAGE exists.")
+    if os.path.isdir("./data"):
+      print("Folder DATA exists.")
+    else
+      print("Creating folder DATA.")
+      mkdirs("./data")
+  else:
+    print("Creating folder PAGE.")
+    mkdirs("./page")
+    if os.path.isdir("./data"):
+      print("Folder DATA exists.")
+    else
+      print("Creating folder DATA.")
+      mkdirs("./data")
+
+def buildPages():
+  exists1,var1 = Argument("-bp","--buildPages",sys.argv)
+  if var1 == True:
+    for file in glob.glob("./data/*.json"):
+      d = json.loads(open(file,"r"))
+      for page in glob.glob("./page/*.py"):
+        x = open(page[:-3] + ".cgi",w+)
+        x.write(open(page,"r").read())
